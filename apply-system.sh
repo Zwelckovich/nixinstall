91053f5c45c85294a4ffcdbee2be1bf55492c37f
Sven Engels <zwelchmobile@googlemail.com>
2023-10-23 17:32:00 +0200

    fix

--- apply-system.sh
+++ apply-system.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 pushd ~/.dotfiles
+# find ~/nix-config/ -type f -name "*.nix" -execdir nixpkgs-fmt {} \;
+# sudo /run/current-system/bin/switch-to-configuration boot
+# sudo nix-collect-garbage -d
+sudo nix flake update
 sudo nixos-rebuild switch --flake .#zwelchnix
 popd
